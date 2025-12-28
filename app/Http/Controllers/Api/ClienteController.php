41c41a66e8a34c2cec6df4fe081774c7287f863d
abrahamalanya <alanyaoficial@gmail.com>
2025-12-28 14:04:58 -0500

    feat: api cliente

---
+++ app/Http/Controllers/Api/ClienteController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+    public function byRuc($ruc)
+    {
+        $cliente = Cliente::where('ruc', $ruc)->first();
+
+        if (!$cliente) {
+            return response()->json([
+                'message' => 'Cliente no encontrado'
+            ], 404);
+        }
+
+        return response()->json([
+            'id' => $cliente->id,
+            'ruc' => $cliente->ruc,
+            'razon_social' => $cliente->razon_social,
+            'estado' => $cliente->estado,
+            'condicion' => $cliente->condicion,
+            'actividad_economica' => $cliente->actividad_economica,
+            'ciudad' => $cliente->ciudad,
+            'departamento_codigo' => $cliente->departamento_codigo,
+            'provincia_codigo' => $cliente->provincia_codigo,
+            'distrito_codigo' => $cliente->distrito_codigo,
+        ]);
+    }
+}
